262c188a6a3b65d5c48851624b414f6953a950b3
AlfredoSequeida <alfredosequeida@outlook.com>
2019-06-25 00:56:57 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,16 @@ elif 'win32' in platform:
 elif 'darwin' in platform:
     platform_file = 'venus/os_tools/darwin.py' 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='venuspy',
     version='0.1.1',
     author="Alfredo Sequeida",
     description='A cross platform tool for setting a random wallpaper image from unsplash.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/AlfredoSequeida/venus',
     download_url='https://github.com/AlfredoSequeida/venus/archive/0.1.1.tar.gz',
     license='MIT',
