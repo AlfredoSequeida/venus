7ab25b314f8dee6fc451d1606cbefc6b98ca37e9
AlfredoSequeida <alfredosequeida@outlook.com>
2019-06-29 20:40:50 -0700

    fixed install for non linux systems

--- setup.py
+++ setup.py
@@ -1,30 +1,18 @@
 from setuptools import setup, find_packages
 import os
-import sys
-
-platform = sys.platform
-
-platform_file = ''
-
-if 'linux' in platform:
-    platform_file = 'venus/os_tools/linux.py' 
-elif 'win32' in platform:
-    platform_file = 'venus/os_tools/windows.py' 
-elif 'darwin' in platform:
-    platform_file = 'venus/os_tools/darwin.py' 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='venuspy',
-    version='0.1.6',
+    version='0.1.7',
     author="Alfredo Sequeida",
     description='A cross platform tool for setting a random wallpaper image from unsplash.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/AlfredoSequeida/venus',
-    download_url='https://github.com/AlfredoSequeida/venus/archive/0.1.6.tar.gz',
+    download_url='https://github.com/AlfredoSequeida/venus/archive/0.1.7.tar.gz',
     keywords ='wallpaper unsplash randomwallpaper',
     platforms ='any',
     classifiers=[
@@ -44,7 +32,11 @@ setup(
         ],
     license='MIT',
     packages=['venus'],
-    scripts=[platform_file],
+    scripts=[
+        'venus/os_tools/linux.py',
+        'venus/os_tools/darwin.py',
+        'venus/os_tools/windows.py',
+        ],
     include_package_data=True,
     install_requires=[
         'requests',
--- venus/__init__.py
+++ venus/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 dir_path_to_conf = os.path.join(os.path.expanduser('~'), '.config/venus')
 
