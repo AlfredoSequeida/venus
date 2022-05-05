6b744d075a8a6cdd54ce97a5c48a17e84fe7d02c
faspaulo <faspaulo@amazon.com>
2022-05-05 09:13:27 -0700

    refactoring and type hints

--- venus/venus.py
+++ venus/venus.py
@@ -1,36 +1,46 @@
-import sys
-import requests
-import tempfile
-import os
 import configparser
-import time
 import glob
+import os
+import sys
+import tempfile
+import time
+
+import requests
 
 
-def get_wall(resolution="1920x1080", search_term=None, output_path=None, collection_id=None):
+def get_url(collection_id: str, resolution: str, search_term: str) -> str:
     """
-    This method downloads a random  wallpaper to use from unsplash. The file is 
-    saved to a temporary file so it can be taken care of by the operating 
+    This method returns unsplash url based on user settings.
+    If  a collection id is provided, it will return images within a given collection that fit the user monitor resolution.
+    Otherwise, it will return random images from Unsplash that match any search criteria provided.
+
+    :param collection_id: string containing unsplash collection id.
+    :param resolution: string containg monitor resolution.
+    :param search_term: string containing search parameters.
+    :return: unsplash url in string format.
+    """
+    if not collection_id:
+        return f"https://source.unsplash.com/{resolution}/?{search_term}"
+
+    return f"https://source.unsplash.com/collection/{collection_id}/{resolution}"
+
+
+def get_wall(base_url: str, output_path: str):
+    """
+    This method downloads a random  wallpaper to use from unsplash. The file is
+    saved to a temporary file so it can be taken care of by the operating
     system.
 
-    :param resolution - the resolution to use for retrieving the wallpaper the  
+    :param resolution: the resolution to use for retrieving the wallpaper the
                         default resolution is 1920x1080
-    :return - the picture file retrieved
+    :return: the picture file retrieved
     """
-
-    # If collection_id is empty, then get images based on search_term
-    if collection_id == '':
-        base_url = 'https://source.unsplash.com/' + resolution + '/?' + search_term
-    # Get images from a collection
-    else:
-        base_url = 'https://source.unsplash.com/collection/' + collection_id + '/' + resolution
 
     r = requests.get(base_url)
 
-    fd, picture_file = tempfile.mkstemp(
-        suffix=".jpg", prefix="venus_", dir=output_path)
+    fd, picture_file = tempfile.mkstemp(suffix=".jpg", prefix="venus_", dir=output_path)
 
-    with os.fdopen(fd, 'wb') as f:
+    with os.fdopen(fd, "wb") as f:
         # downloading the image to the system
         f.write(r.content)
 
@@ -45,7 +55,7 @@ def get_config():
     """
 
     config = configparser.ConfigParser()
-    config.read(os.path.join(os.path.expanduser('~'), '.config/venus/config'))
+    config.read(os.path.join(os.path.expanduser("~"), ".config/venus/config"))
 
     return config
 
@@ -54,23 +64,24 @@ def main():
     # checking platform for using system specific code
     platform = sys.platform
 
-    if 'linux' in platform:
+    if "linux" in platform:
         from venus.os_tools import linux as system
-    elif 'win32' in platform:
+    elif "win32" in platform:
         from venus.os_tools import windows as system
-    elif 'darwin' in platform:
+    elif "darwin" in platform:
         from venus.os_tools import darwin as system
 
     # getting config
     config = get_config()
-    search_term_config = ",".join(sys.argv[1:]) or config.get('SETTINGS', 'SEARCH_TERMS', fallback='')
-    screen_resolution_config = config.get('SETTINGS', 'SCREEN_RESOLUTION', fallback='')
-    output_path_config = config.get('SETTINGS', 'OUTPUT_PATH', fallback='')
-    wait_time_config = config.get('SETTINGS', 'WAIT_TIME', fallback=0)
-    cache_items_config = config.get('SETTINGS', 'CACHE_ITEMS', fallback=0)
-    use_pywal_config = config.getboolean(
-        'SETTINGS', 'USE_PYWAL', fallback=False)
-    collection_id_config = config.get('SETTINGS', 'COLLECTION_ID', fallback='')
+    search_term_config = ",".join(sys.argv[1:]) or config.get(
+        "SETTINGS", "SEARCH_TERMS", fallback=""
+    )
+    screen_resolution_config = config.get("SETTINGS", "SCREEN_RESOLUTION", fallback="")
+    output_path_config = config.get("SETTINGS", "OUTPUT_PATH", fallback="")
+    wait_time_config = config.get("SETTINGS", "WAIT_TIME", fallback=0)
+    cache_items_config = config.get("SETTINGS", "CACHE_ITEMS", fallback=0)
+    use_pywal_config = config.getboolean("SETTINGS", "USE_PYWAL", fallback=False)
+    collection_id_config = config.get("SETTINGS", "COLLECTION_ID", fallback=None)
 
     # default path for empty OUTPUT_PATH setting
     if not output_path_config:
@@ -80,17 +91,24 @@ def main():
     if not screen_resolution_config:
         screen_resolution_config = system.get_screen_resolution()
 
+    base_url_config = get_url(
+        collection_id_config, screen_resolution_config, search_term_config
+    )
+
     # loop control var
     run = True
 
     while run:
 
-        system.set_wall(get_wall(resolution=screen_resolution_config,
-                                 search_term=search_term_config,
-                                 output_path=output_path_config,
-                                 collection_id=collection_id_config), use_pywal_config)
+        system.set_wall(
+            get_wall(
+                base_url=base_url_config,
+                output_path=output_path_config,
+            ),
+            use_pywal_config,
+        )
         if cache_items_config and output_path_config:
-            cacheFiles = glob.glob(output_path_config+"/*")
+            cacheFiles = glob.glob(output_path_config + "/*")
             cacheFiles.sort(key=os.path.getmtime, reverse=True)
             if len(cacheFiles) >= int(cache_items_config):
                 for i, file in enumerate(cacheFiles):
